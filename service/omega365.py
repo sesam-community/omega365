30238f29d46c5b6069f7c0b1d95ec381872fbf32
Tarjei Skrede <tarjeiskrede@M-C02Y7599JGH6.local>
2021-04-22 11:49:04 +0200

    Change from posting payload to a url system approach

--- service/omega365.py
+++ service/omega365.py
@@ -4,6 +4,7 @@ import os
 import logger
 import cherrypy
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 logger = logger.Logger('Omega365 client service')
@@ -13,6 +14,9 @@ username = os.environ.get("username")
 pw = os.environ.get("password")
 remove_namespaces = os.environ.get("remove_namespaces", True)
 headers = json.loads('{"Content-Type": "application/json"}')
+resources_config = json.loads(os.environ.get("resources", '[]'))
+
+resources = {}
 
 
 class BasicUrlSystem:
@@ -44,7 +48,7 @@ def authenticate(s):
         logger.warning("Exception occurred when authenticating the user: '%s'", e)
 
 
-def stream_json(clean):
+def stream_json(clean, since_property_name, id_property_name):
     first = True
     yield '['
     for i, row in enumerate(clean):
@@ -52,6 +56,10 @@ def stream_json(clean):
             yield ','
         else:
             first = False
+        if since_property_name is not None:
+            row["_updated"] = row[since_property_name]
+        if id_property_name is not None:
+            row["_id"] = str(row[id_property_name])
         yield json.dumps(row)
     yield ']'
 
@@ -67,6 +75,154 @@ def remove_ns(keys):
                 keys[new_key] = keys.pop(key)
         for val in keys.values():
             remove_ns(val)
+
+
+def populate_resources():
+    for resource in resources_config:
+        since_property_name = None
+        id_property_name = None
+        if "since_property_name" in resource:
+            since_property_name = resource["since_property_name"]
+        if "id_property_name" in resource:
+            id_property_name = resource["id_property_name"]
+        resources[resource["resource_name"]] = \
+            {
+                "fields": resource["fields"],
+                "since_property_name": since_property_name,
+                "id_property_name": id_property_name
+            }
+
+
+@app.route("/<path:path>", methods=["GET"])
+def get(path):
+    request_url = "{0}{1}".format(url, "/api/data")
+    logger.info("Request url: %s", request_url)
+
+    if path not in resources:
+        raise Exception("Resource with name '{0}' not found!".format(path))
+
+    where_clause = None
+    if request.args.get('since') is not None and resources[path]["since_property_name"] is not None:
+        where_clause = "{0} >= '{1}'".format(resources[path]["since_property_name"], datetime.strptime(request.args.get('since'), "%Y-%m-%dT%H:%M.%S"))
+
+    request_data = {
+        "maxRecords": -1,
+        "operation": "retrieve",
+        "resourceName": path,
+        "fields": resources[path]["fields"],
+        "whereClause": where_clause
+    }
+
+    logger.info("Request data: %s", request_data)
+
+    with session_factory.make_session() as s:
+        authenticate(s)
+
+        response = s.request("POST", request_url, json=request_data, headers=headers)
+
+        if response.status_code != 200:
+            raise Exception(response.reason + ": " + response.text)
+
+        result = json.loads(response.text)
+
+    return Response(
+        stream_json(result['success'], resources[path]["since_property_name"], resources[path]["id_property_name"]),
+        mimetype='application/json'
+    )
+
+
+@app.route("/<path:path>", methods=["POST"])
+def post(path):
+    request_url = "{0}{1}".format(url, "/api/data")
+    logger.info("Post request url: %s", request_url)
+
+    if path not in resources:
+        raise Exception("Resource with name '{0}' not found!".format(path))
+
+    request_data = request.get_json()
+
+    logger.info("Post request data: %s", request_data)
+
+    create_template = {
+        "maxRecords": -1,
+        "operation": "create",
+        "resourceName": path,
+        "uniqueName": path,
+        "excludeFieldNames": False,
+        "fields": resources[path]["fields"]
+    }
+
+    def generate(entities):
+        yield "["
+        with session_factory.make_session() as s:
+            authenticate(s)
+            for index, entity in enumerate(entities):
+                if index > 0:
+                    yield ","
+
+                create_entity = entity.copy()
+                create_entity.update(create_template)
+
+                response = s.request("POST", request_url, json=create_entity, headers=headers)
+
+                if response.status_code != 200:
+                    logger.warning("An error occurred: {0}. {1}".format(response.reason, response.text))
+                    raise Exception(response.reason + ": " + response.text)
+
+                result = json.loads(response.text)
+
+                yield json.dumps(result['success'])
+        yield "]"
+
+    response_data_generator = generate(request_data)
+    response_data = response_data_generator
+    return Response(response=response_data, mimetype="application/json")
+
+
+@app.route("/<path:path>", methods=["DELETE"])
+def delete(path):
+    request_url = "{0}{1}".format(url, "/api/data")
+    logger.info("Delete request url: %s", request_url)
+
+    if path not in resources:
+        raise Exception("Resource with name '{0}' not found!".format(path))
+
+    request_data = request.get_json()
+
+    logger.info("Delete request data: %s", request_data)
+
+    delete_template = {
+        "operation": "destroy",
+        "resourceName": path,
+        "uniqueName": path
+    }
+
+    def destroy(entities):
+        yield "["
+        with session_factory.make_session() as s:
+            authenticate(s)
+            for index, entity in enumerate(entities):
+                if index > 0:
+                    yield ","
+
+                delete_entity = entity.copy()
+                delete_entity.update(delete_template)
+
+                #response = s.request("POST", request_url, json=delete_entity, headers=headers)
+
+                #if response.status_code != 200:
+                #    logger.warning("An error occurred: {0}. {1}".format(response.reason, response.text))
+                #    raise Exception(response.reason + ": " + response.text)
+
+                #result = json.loads(response.text)
+
+                #yield json.dumps(result['success'])
+                yield json.dumps([])
+        yield "]"
+
+    response_data_generator = destroy(request_data)
+    response_data = response_data_generator
+    return Response(response=response_data, mimetype="application/json")
 
 
 @app.route("/retrieve", methods=["POST"])
@@ -133,22 +289,10 @@ def create():
     return Response(response=response_data, mimetype="application/json")
 
 
-@app.route("/delete", methods=["DELETE"])
-def delete():
-    request_url = "{0}{1}".format(url, "/api/data")
-    logger.info("Request url: %s", request_url)
-
-    request_data = request.get_json()
-
-    if remove_namespaces:
-        remove_ns(request_data)
-
-    logger.info("Request data: %s", request_data)
-
-    return None
-
 if __name__ == '__main__':
     cherrypy.tree.graft(app, '/')
+
+    populate_resources()
 
     # Set the configuration of the web server to production mode
     cherrypy.config.update({
